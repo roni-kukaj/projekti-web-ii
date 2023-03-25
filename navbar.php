19461045565f12233b788a23feb18cddef57b45a
getuarkelmendi <102876616+getuarkelmendi@users.noreply.github.com>
2023-03-25 21:53:11 +0100

    test2

--- navbar.php
+++ navbar.php
@@ -5,8 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/navbar.css">
 </head>
 <body>
-    <p>Test</p>
+    <nav>
+        <ul>
+            <li><a href="">Shop</a></li>
+            <li><a href="">Categories</a></li>
+            <li><a href="">Pages</a></li>
+            <li><a href="">Elements</a></li>
+        </ul>
+    </nav>
+    <p>Testa</p>
+    <p>asdasdasds</p>
 </body>
 </html>
