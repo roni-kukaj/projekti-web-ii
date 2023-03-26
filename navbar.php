2058b0504a0189ad30747c8f8b9bccd4ce8450a8
getuarkelmendi <102876616+getuarkelmendi@users.noreply.github.com>
2023-03-26 23:53:57 +0200

    navbar created

--- navbar.php
+++ navbar.php
@@ -6,17 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../css/navbar.css">
+    <script src="https://kit.fontawesome.com/26e97bbe8d.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <nav>
-        <ul>
-            <li><a href="">Shop</a></li>
-            <li><a href="">Categories</a></li>
-            <li><a href="">Pages</a></li>
-            <li><a href="">Elements</a></li>
-        </ul>
-    </nav>
-    <p>Testa</p>
-    <p>asdasdasds</p>
+
+    <nav class="navbar">
+		<div class="navbar-content">
+			<a href="index.html">
+				LOGO
+			</a>
+			<div class="middle">
+				<a href="#" class="link">Home</a>
+				<a href="#" class="link">Shop</a>
+				<a href="#" class="link">Categories</a>
+			</div>
+			<div class="right">
+				<a href="#" class="link">Sign In</a>
+				<a href="#" class="link" id="cart"><i class="fa fa-shopping-cart" style="font-size:22px"></i></a>
+			</div>
+			<i class="fa-solid fa-bars-staggered" id="burger-menu"></i>
+		</div>
+	</nav>
+
+    <script>
+        $(document).ready(() => {
+	// Navbar links dropdown
+	$("#burger-menu").click(() => {
+		$(".middle").toggle();
+		$(".right").toggle();
+	});
+});
+    </script>
 </body>
 </html>
