468c7081f46502a1236096b19132ccaffca05b85
Lorena Mekaj <lorenamekaj@gmail.com>
2023-04-23 20:26:40 +0200

    html to php

--- index.php
+++ index.php
@@ -1,5 +1,161 @@
 <?php require("includes/repetitions/header.php"); ?>
 
-<!-- Ketu mund te shkruani kodin HTML -->
+ <!-- Start Banner Hero -->
+    <div id="template-mo-zay-hero-carousel" class="carousel slide" data-bs-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="1"></li>
+            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="container">
+                    <div class="row p-2">
+                        <div class="mx-auto col-md-10 col-lg-6 order-lg-last" >
+                            <img class="img-fluid" id="nike-shoe" src="./assets/img/nike3_thumbnail.png" alt="">
+                        </div>
+                        <div class="col-lg-6 mb-0 d-flex align-items-center">
+                            <div class="text-align-left align-self-center">
+                                <h1 class="h1 text-success"></h1>
+                                <h3 class="h2">Nike</h3>
+                                <p>
+                                    Nike, is an American multinational corporation that is engaged in the design, development, manufacturing, and worldwide marketing and sales of footwear, apparel, equipment, accessories, and services. <a rel="sponsored" class="text-success" href="https://stories.freepik.com/" target="_blank">Freepik Stories</a>,
+                                    <a rel="sponsored" class="text-success" href="https://www.nike.com/" target="_blank">Nike</a> and
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="container">
+                    <div class="row p-5">
+                        <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+                            <img class="img-fluid" id="puma-shoe" src="./assets/img/puma_thumbnail.png" alt="">
+                        </div>
+                        <div class="col-lg-6 mb-0 d-flex align-items-center">
+                            <div class="text-align-left">
+                                <h1 class="h1"></h1>
+                                <h3 class="h2">Puma</h3>
+                                <p>
+                                    Puma SE, branded as PUMA, is a German multinational corporation that designs and manufactures athletic and casual footwear, apparel and accessories, which is headquartered in Herzogenaurach, Bavaria, Germany.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="container">
+                    <div class="row p-5">
+                        <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
+                            <img class="img-fluid" id="vans-shoe" src="./assets/img/vans_thumbnail.png" alt="">
+                        </div>
+                        <div class="col-lg-6 mb-0 d-flex align-items-center">
+                            <div class="text-align-left">
+                                <h1 class="h1"></h1>
+                                <h3 class="h2">Vans </h3>
+                                <p>
+                                    Vans is an American manufacturer of skateboarding shoes and related apparel, established in Anaheim, California, and owned by VF Corporation.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev text-decoration-none w-auto ps-3" href="#template-mo-zay-hero-carousel" role="button" data-bs-slide="prev">
+            <i class="fas fa-chevron-left"></i>
+        </a>
+        <a class="carousel-control-next text-decoration-none w-auto pe-3" href="#template-mo-zay-hero-carousel" role="button" data-bs-slide="next">
+            <i class="fas fa-chevron-right"></i>
+        </a>
+    </div>
+    <!-- End Banner Hero -->
+
+
+    <!-- Start Categories of The Month -->
+    <section class="container py-5">
+        <div class="row text-center pt-3">
+            <div class="col-lg-6 m-auto">
+                <h1 class="h1">Categories of The Month</h1>
+                
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-3 p-3 mt-3 mx-3">
+                <a href="#"><img src="./assets/img/nikr.webp" class="rounded-circle border"></a>
+                <h5 class="text-center mt-3 mb-3">Sneakers</h5>
+                <p class="text-center"><a class="btn btn-success">Go Shop</a></p>
+            </div>
+            <div class="col-12 col-md-3 p-3 mt-3 mx-3">
+                <a href="#"><img src="./assets/img/htop.jpg" class="rounded-circle border"></a>
+                <h2 class="h5 text-center mt-3 mb-3">High-tops</h2>
+                <p class="text-center"><a class="btn btn-success">Go Shop</a></p>
+            </div>
+            <div class="col-12 col-md-3 p-3 mt-3 mx-3">
+                <a href="#"><img src="./assets/img/boots.jpg" class="rounded-circle border"></a>
+                <h2 class="h5 text-center mt-3 mb-3">Boots</h2>
+                <p class="text-center"><a class="btn btn-success">Go Shop</a></p>
+            </div>
+        </div>
+    </section>
+    <!-- End Categories of The Month -->
+
+
+    <!-- Start Featured Product -->
+    <section class="bg-light">
+        <div class="container py-5">
+            <div class="row text-center py-3">
+                <div class="col-lg-6 m-auto">
+                    <h1 class="h1">Featured Product</h1>
+                    <p>
+                        
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card h-100">
+                        <a href="shop-single.html">
+                            <img src="./assets/img/nbs.png" class="card-img-top" alt="...">
+                        </a>
+                        <div class="card-body">
+                            <a href="shop-single.html" class="h2 text-decoration-none text-dark">New Balance</a>
+                            <p class="card-text">
+                                New Balance Athletics, Inc. (NB), best known as simply New Balance, is one of the world's major sports footwear and apparel manufacturers.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card h-100">
+                        <a href="shop-single.html">
+                            <img src="./assets/img/adidas.png" class="card-img-top mb-3" alt="...">
+                        </a>
+                        <div class="card-body">
+                            <a href="shop-single.html" class="h2 text-decoration-none text-dark">Adidas</a>
+                            <p class="card-text">
+                                Adidas AG is a German multinational corporation, founded and headquartered in Herzogenaurach, Bavaria, that designs and manufactures shoes, clothing and accessories. It is the largest sportswear manufacturer in Europe
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card h-100">
+                        <a href="shop-single.html">
+                            <img src="./assets/img/dr.jpg" class="card-img-top mb-5" alt="...">
+                        </a>
+                        <div class="card-body">
+                            <a href="shop-single.html" class="h2 text-decoration-none text-dark">Dr Martens</a>
+                            <p class="card-text">
+                                Dr. Martens, also commonly known as Doc Martens, Docs or DMs, is a German-founded British footwear and clothing brand, headquartered in Wollaston in the Wellingborough district of Northamptonshire, England. 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     
 <?php require("includes/repetitions/footer.php"); ?>
