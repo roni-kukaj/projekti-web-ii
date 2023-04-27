1207397b8b80cbfaf3d086e1795e96cf55bea59e
MajlindaHalilaj <121357662+MajlindaHalilaj@users.noreply.github.com>
2023-04-27 22:47:27 +0200

    changes

--- FAQ.php
+++ FAQ.php
@@ -1,5 +1,86 @@
 <?php require("includes/repetitions/header.php"); ?>
 
-<!-- Ketu mund te shkruani kodin HTML -->
+<section class="faq-section container">
+            <div class="shopping-div">
+                <h2 class="shopping-questions">Shipping</h2>
+                <ul class="questions-list">
+                    <li class="question"><p>What Shipping Methods Are Available?</p></li>
+                    <li class="answer"><p>Our shipping methods depend on the location of the costumers. At the 
+                    moment we do <mark>Standard</mark> and <mark>Internation</mark> shipping.</p></li>
+                </ul>
+                <ul class="questions-list">
+                    <li class="question second-que"><p>Do You Ship Internationally?</p></li>
+                    <li class="answer second-ans"><p>We do ship internationally. Our international shippings right
+                        now are only available in Europe but we are looking forward to do shipping all over the
+                        world.</p></li>
+                </ul>
+                <ul class="questions-list">
+                    <li class="question second-que"><p>How Long Will It Take To Get My Package?</p></li>
+                    <li class="answer second-ans"><p>The shipping time can vary depending on the shipping method
+                        our clients choose. A Standard shipping takes <mark>3-5 business days</mark>, whereas an
+                        international shipping can take up to <mark>7 business days.</mark></p></li>
+                </ul>
+            </div>
+            <hr class="hr-line">
+            <div class="shopping-div">
+                <h2 class="shopping-questions">Payment</h2>
+                <ul class="questions-list">
+                    <li class="question"><p>What Payment Methods Are Accepted?</p></li>
+                    <li class="answer"><p>We offer <mark>Credit/debit card</mark>, <mark>PayPal</mark> and
+                        <mark>Bank transfer</mark> payment.</p></li>
+                </ul>
+                <ul class="questions-list">
+                    <li class="question second-que"><p>Is Buying On-Line Safe?</p></li>
+                    <li class="answer second-ans"><p>Our shop is a very safe to buy online. We take certain precautions
+                        to protect your personal information and financial transactions.</p></li>
+                </ul>
+            <hr class="hr-line">
+            <div class="shopping-div">
+                <h2 class="shopping-questions">Orders & Returns</h2>
+                <ul class="questions-list">
+                    <li class="question"><p>How do I place an Order?</p></li>
+                    <li class="answer"><p>Here are some steps you can follow:
+                        <ol>
+                            <li>Go to the Shop page or search the product you want to buy.
+                                You can usually search for products by category, brand, or keyword.</li>
+                            <li>Select the product(s) you want to purchase and add them to your shopping cart.
+                                You can usually adjust the quantity of the product(s) in the shopping cart if
+                                needed.</li>
+                            <li>Once you have added all the products you want to purchase to your shopping cart,
+                                click on the checkout button.</li>
+                            <li>Enter your personal and shipping information, including your name, address, email 
+                                and phone number.</li>
+                            <li>Select your preferred payment method and enter your payment details.</li>
+                            <li>Click on the submit or place order button to complete your purchase.</li>
+                        </ol>
+                    </p></li>
+                </ul>
+                <ul class="questions-list">
+                    <li class="question second-que"><p>How Can I Cancel Or Change My Order?</p></li>
+                    <li class="answer second-ans"><p>Our shop does not allow cancellations or changes after the 
+                        order has been processed or shipped. You may need to wait until you receive the order and
+                        then follow our return or exchange policy.</p></li>
+                </ul>
+                <ul class="questions-list">
+                    <li class="question second-que"><p>How Can I Return a Product?</p></li>
+                    <li class="answer second-ans"><p>Here are some steps you can follow:
+                        <ol>
+                            <li>Contact our customer service as soon as possible to initiate the return process.
+                                You can find our contact information on the website or in the order confirmation
+                                email.</li>
+                            <li>Provide the customer service representative with your order number and explain why
+                                you want to return the product. Be specific about the reason for the return, such
+                                as a defect or an incorrect item.</li>
+                            <li>Follow the instructions provided by the customer service representative. They may
+                                ask you to fill out a return request form, or they may provide you with a shipping
+                                label and return instructions.</li>
+                            <li>Once we receive the returned product and verify its condition, we will provide you
+                                with a refund or exchange.</li>
+                        </ol>
+                    </p></li>
+                </ul>
+            </div>
+            </div>
+        </section>
     
 <?php require("includes/repetitions/footer.php"); ?>
