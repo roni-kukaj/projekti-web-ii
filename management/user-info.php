318eb546444ddf4148fb33efa7966fae17883618
roni-kukaj <roni.kukaj@student.uni-pr.edu>
2023-04-29 00:00:44 +0200

    pic changes

--- management/user-info.php
+++ management/user-info.php
@@ -32,3 +32,11 @@ if(!isset($_SESSION['username']) && !isset($_SESSION['role'])){
 
 
 <?php require "includes/repetitions/footer.php"; ?>
+
+<!--
+ALTER TABLE `products` ADD COLUMN picture VARCHAR(512) AFTER `price`;
+INSERT INTO `products`(`name`, `gender`, `size`, `quantity`, `price`, `picture`) VALUES ('Dr. Martens Classic', 'Female', 38, 10, 119.99, 'assets/images/products/boots_1.jpg');
+INSERT INTO `products`(`name`, `gender`, `size`, `quantity`, `price`, `picture`) VALUES ('Nike Sneaker', 'Female', 39, 14, 69.99, 'assets/images/products/nike_1.jpg');
+INSERT INTO `products`(`name`, `gender`, `size`, `quantity`, `price`, `picture`) VALUES ('Vans High Tops', 'Female', 37, 9, 99.99, 'assets/images/products/vans_1.jpg');
+
+-->
