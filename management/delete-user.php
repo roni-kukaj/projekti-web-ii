3b80237070a5fbcf8ee6c325b2d9691259b9a3f6
roni-kukaj <roni.kukaj@student.uni-pr.edu>
2023-04-29 01:56:11 +0200

    deletion

--- management/delete-user.php
+++ management/delete-user.php
@@ -6,20 +6,20 @@ if(!isset($_SESSION['username']) && !isset($_SESSION['role'])){
 <script>
         window.location.replace('index.php');
 </script>
-<?php } else if (!isset($_GET['user_id'])) { ?>
+<?php } else if (!isset($_GET['id'])) { ?>
     <script>
         window.location.replace('users.php');
     </script>
 <?php } ?>
 
 <?php
-    $sql = "DELETE * FROM users WHERE id = ".$_GET['user_id'];
+    $sql = "DELETE * FROM users WHERE id = {$_GET['id']}";
     if(mysqli_query($conn, $sql)){
         header("Location: users.php");
         exit();
     }
     else{
-        header("Location: users.php?error=The action could not be performed!");
+        header("Location: user-info.php?id={$_GET['id']}&error=The action could not be performed!");
         exit();
     }
 ?>
