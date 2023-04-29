2977a122305e03e9f0b9974ddb6f84aeeb1516e4
roni-kukaj <roni.kukaj@student.uni-pr.edu>
2023-04-29 23:24:08 +0200

    Signup finalized

--- includes/sessions/signup.php
+++ includes/sessions/signup.php
@@ -3,33 +3,29 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     session_start();
 
     include "../db/db_connection.php";
+    require "../validation/validation.php";
 
-    $name = $_POST['name'];
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $confirm_password = $_POST['confirm_password'];
+    $errors = array();
 
-    if(isset($_POST['name']) && isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['confirm_password'])){
-        function validate($data) {
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
-        }
+    function validate($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
-    $name = validate($_POST['name']);
-    $username = validate($_POST['username']);
+
+    $firstname = validate($_POST['firstname']);
+    $lastname = validate($_POST['lastname']);
     $email = validate($_POST['email']);
     $password = validate($_POST['password']);
-    $confirm_password = validate($_POST['confirm_password']);
+    $confirm_password = validate($_POST['confirmpassword']);
 
-    if(empty($name)){
-        header("Location: ../../signup-page.php?error=Name is empty!");
+    if(empty($firstname)){
+        header("Location: ../../signup-page.php?error=Firstname is empty!");
         exit();
     }
-    if(empty($username)){
-        header("Location: ../../signup-page.php?error=Username is empty!");
+    if(empty($lastname)){
+        header("Location: ../../signup-page.php?error=Lastname is empty!");
         exit();
     }
     if(empty($email)){
@@ -44,23 +40,41 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         header("Location: ../../signup-page.php?error=Confirm Password is empty!");
         exit();
     } 
-    if($_POST['password'] !== $_POST['confirm_password']){
-        header("Location: ../../signup-page.php?error=Passwords don't match!");
+    if($_POST['password'] !== $_POST['confirmpassword']){
+        $errors['confirmpassword_error'] = "Passwords don't match!";
+    }
+
+    if(!validate_firstname($firstname)){
+        $errors['firstname_error'] = "The firstname is not valid";
+    }
+    if(!validate_lastname($lastname)){
+        $errors['lastname_error'] = "The lastname is not valid";
+    }
+    if(!validate_email($email)){
+        $errors['email_error'] = "The email is not valid!";
+    }
+    if(!validate_password($password)){
+        $errors['password_error'] = "The password must include lowercase characters, uppercase characters, numbers and symbols!";
+    }
+
+    if(!empty($errors)){
+        $query_string = http_build_query($errors);
+        $url = "../../signup-page.php?".$query_string;
+        header("Location: {$url}");
         exit();
     }
-}
 
-    
-    $sql = "INSERT INTO users (emri, mbiemri, email, password) VALUES ('$name', '$username', '$email', '$password')";
+    $password = password_hash($password, PASSWORD_BCRYPT);
+
+    $sql = "INSERT INTO users (emri, mbiemri, email, password) VALUES ('$firstname', '$lastname', '$email', '$password')";
     $result = mysqli_query($conn, $sql);
 
     if($result){
-        header("Location: ../../signup-page.php");
+        header("Location: ../../login-page.php");
         exit();
     } else{
         header("Location: ../../signup-page.php?error=Failed to insert data!");
         exit();
     }
-
-
+}
 ?>
--- signup-page.php
+++ signup-page.php
@@ -1,57 +1,46 @@
 <?php require("includes/repetitions/header.php"); ?>
 
-<html>
-    <head>
-        <title>Sign up</title>
-     </head>
-    <body>
-
-        <br><br>
-
-        <div class="container mt-5">
-        <div class="row">
-            <div class="col mx-1">
-                <img src="assets/images/signup.avif" alt="Crypto" height="468px" width="600px" style="border-radius:20px;" >
-            </div>
-
-            <div class="col mx-2">
-                <form  id="signup-form" class="my-1" action="includes/sessions/signup.php" method="POST">
-
-                            <label class="form-label my-1" for=""><b>Name</b></label>
-                             <input  class="form-control my-1" type="text" class="input" name="name" required>
-
-
-                            <label class="form-label my-1" for="username"><b>Username</b> </label>
-                           <input class="form-control my-1" type="text" class="input" name="username" required>
-                            <label class="form-label my-1" for="ID"><b>ID number</b></label>
-                             <input class="form-control my-1" type="text"  class="input">
-
-                             <label class="form-label my-1" for="email"><b>Email</b></label>
-                             <input class="form-control my-1" type="text" name="email" class="input">
-
-                            <label class="form-label my-1" for="password"><b>Password</b></label>
-                             <input class="form-control my-1" name="password" id="password" type="password"  class="input">
-
-                            <label class="form-label my-1" for="confirm"><b>Confirm Password</b></label>
-                            <input class="form-control my-1" type="password" name="confirm_password" id="confirm_password" class="input" required>
-
-                            <p id="signup-error-msg" style="opacity:0;">Your passwords don't match.</p>
-                            <p id="login-error-msg" class="mt-1 text-danger" style="display: block;">
-                            <?php 
-                                if(isset($_GET['error'])){
-                                    echo $_GET['error'];
-                                }
-                            ?>
-                            </p>
-                        <button type="submit" class="login btn btn-secondary my-1 " id="signup-form-submit">Sign Up</button>
-
-                       <p style="text-align:center; color:black;">Already have an accont? <a href="login-page.php" class="link">Log in</a> now</p>
-                    </form>
-                </div>
-            </div>
+<div class="container my-5">
+    <div class="row">
+        <div class="col-md-5 d-flex align-items-center">
+            <img src="assets/images/signup.png" alt="Crypto" class="img w-100">
         </div>
+        <div class="col-md-7">
+            <form  id="signup-form" class="my-1" action="includes/sessions/signup.php" method="POST">
 
-       </body>
-    <br><br><br><br>
-</html>
+                <label class="form-label my-1" for=""><b>Firstname</b></label>
+                <input  class="form-control my-1" type="text" class="input" name="firstname" required>
+                <p class="text-danger"><?php if(isset($_GET['firstname_error'])) echo $_GET['firstname_error']; ?></p>
 
+                <label class="form-label my-1" for="username"><b>Lastname</b> </label>
+                <input class="form-control my-1" type="text" class="input" name="lastname" required>
+                <p class="text-danger"><?php if(isset($_GET['lastname_error'])) echo $_GET['lastname_error']; ?></p>
+
+                <label class="form-label my-1" for="email"><b>Email</b></label>
+                <input class="form-control my-1" type="text" name="email" class="input" required>
+                <p class="text-danger"><?php if(isset($_GET['email_error'])) echo $_GET['email_error']; ?></p>
+
+                <label class="form-label my-1" for="password"><b>Password</b></label>
+                <input class="form-control my-1" name="password" id="password" type="password"  class="input" required>
+                <p class="text-danger"><?php if(isset($_GET['password_error'])) echo $_GET['password_error']; ?></p>
+                
+                <label class="form-label my-1" for="confirm"><b>Confirm Password</b></label>
+                <input class="form-control my-1" type="password" name="confirmpassword" id="confirmpassword" class="input" required>
+                <p class="text-danger"><?php if(isset($_GET['confirmpassword_error'])) echo $_GET['confirmpassword_error']; ?></p>
+               
+                <p id="login-error-msg" class="mt-1 text-danger" style="display: block;">
+                <?php 
+                    if(isset($_GET['error'])){
+                        echo $_GET['error'];
+                    }
+                ?>
+                </p>
+                <button type="submit" class="login btn btn-success my-1 " id="signup-form-submit">Sign Up</button>
+                <br><br>
+                <p>Already have an account? <a href="login-page.php" class="link-success">Log in</a> now!</p>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php require "includes/repetitions/footer.php"; ?>
