ccd6938e8583e26cc46dfdc45c7c19017e9bd7b2
roni-kukaj <roni.kukaj@student.uni-pr.edu>
2023-05-01 20:45:28 +0200

    minor changes

--- shop.php
+++ shop.php
@@ -89,24 +89,7 @@
                         </div>    
                     </div>
                 <?php $product_count += 1; }?>
-
-                    
-                    
-                <div div="row">
-                    <ul class="pagination pagination-lg justify-content-end">
-                        <li class="page-item disabled">
-                            <a class="page-link active rounded-0 mr-3 shadow-sm border-top-0 border-left-0" href="#" tabindex="-1">1</a>
-                        </li>
-                        <li class="page-item">
-                            <a class="page-link rounded-0 mr-3 shadow-sm border-top-0 border-left-0 text-dark" href="#">2</a>
-                        </li>
-                        <li class="page-item">
-                            <a class="page-link rounded-0 shadow-sm border-top-0 border-left-0 text-dark" href="#">3</a>
-                        </li>
-                    </ul>
-                </div>
             </div>
-
         </div>
     </div>
     <!-- End Content -->
